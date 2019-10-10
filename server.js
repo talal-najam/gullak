88302645b37978f0e1b8b658c2aefd8dff015766
Najam, Talal <Talal.Najam@experian.com>
2019-10-10 21:24:44 +0800

    edit entrypoint

--- server.js
+++ server.js
@@ -29,7 +29,6 @@ app.use('/api/profiles', require('./routes/api/profiles'));
 // if (process.env.NODE_ENV === 'production') {
 // Set static folder
 app.use(express.static('client/build'));
-console.log("I'm in production nigga!")
 
 app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
