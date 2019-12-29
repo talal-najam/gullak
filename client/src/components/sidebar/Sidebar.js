72d39c93c9ca75ac5017342968225a594181109a
Talal Najam <talalnajam98@gmail.com>
2019-12-29 19:28:58 +0800

    Add sidebar buttons, links and their stylings

--- client/src/components/sidebar/Sidebar.js
+++ client/src/components/sidebar/Sidebar.js
@@ -1,11 +1,84 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { Link } from 'react-router-dom';
+import propTypes from 'prop-types';
+import { connect } from 'react-redux';
 
-const Sidebar = () => {
-    return (
-        <h1>
-            Hi I'm a Sidebar
-        </h1>
-    )
+
+const Sidebar = ({ auth: { isAuthenticated, loading } }) => {
+
+    const guestSidebar = (
+        <div className="sidebar-wrapper">
+            <div className="sidebar-branding-container">
+                <Link className="sidebar-branding" to="/">Gullak</Link>
+            </div>
+            <div className="sidebar-items">
+                <ul className="sidebar-items-list">
+                    <li className="sidebar-item">
+                        <Link className="sidebar-nav" to="/about">
+                            About
+                        </Link>
+                    </li>
+                    <li className="sidebar-item">
+                        <a className="sidebar-nav" href="https://github.com/mistat44/gullak" rel="noopener noreferrer" target="_blank" rel="noopener noreferrer" tabIndex="-1" aria-disabled="true">
+                            Github
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    );
+
+    const authSidebar = (
+        <div className="sidebar-wrapper">
+            <div className="sidebar-branding-container">
+                <Link className="sidebar-branding" to="/">Gullak</Link>
+            </div>
+            <div className="sidebar-items">
+                <ul className="sidebar-items-list">
+                    <li className="sidebar-item">
+                        <Link className="sidebar-nav" to="/">
+                            Dashboard
+                        </Link>
+                    </li>
+                    <li className="sidebar-item">
+                        <Link className="sidebar-nav" to="/items">
+                            Items
+                        </Link>
+                    </li>
+                    <li className="sidebar-item">
+                        <Link className="sidebar-nav" to="/transactions">
+                            Transactions
+                        </Link>
+                    </li>
+                    <li className="sidebar-item">
+                        <Link className="sidebar-nav" to="/about">
+                            About
+                        </Link>
+
+                    </li>
+                    <li className="sidebar-item">
+                        <a className="sidebar-nav" href="https://github.com/mistat44/gullak" rel="noopener noreferrer" target="_blank" rel="noopener noreferrer" tabIndex="-1" aria-disabled="true">
+                            Github
+                        </a>
+                    </li>
+                </ul >
+            </div >
+        </div >
+    );
+
+return (
+    <Fragment>
+        {isAuthenticated ? (authSidebar) : (guestSidebar)}
+    </Fragment>
+)
 }
 
-export default Sidebar;
+Sidebar.propTypes = {
+    auth: propTypes.object.isRequired
+}
+
+const mapStateToProps = state => ({
+    auth: state.auth
+})
+
+export default connect(mapStateToProps, {})(Sidebar);
